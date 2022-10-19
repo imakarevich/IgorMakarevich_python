7bce462f1ee9c315951a4a147c9756590e5e0d8b
IgorMakarevich <grigorichok@gmail.com>
2022-10-19 21:43:19 +0300

    CRUDL-end

--- src/app_reference_book/views.py
+++ src/app_reference_book/views.py
@@ -65,4 +65,64 @@ class ListAuthors(generic.ListView):
     model = models.Authors
     template_name = 'app_reference_book/Authors_list.html'
 
-#################################################################################
+###########################CRUDL-Genres#################################
+#CREATE 
+class CreateGenres(generic.CreateView):
+    model = models.Genres
+    form_class = forms.GenresForm
+    template_name = 'app_reference_book/genres_create.html'
+    success_url = '/genres/'
+
+#READ
+class ReadGenres(generic.DetailView):
+    model = models.Genres
+    template_name = 'app_reference_book/genres_read.html'
+
+#UPDATE
+class UpdateGenres(generic.UpdateView):
+    model = models.Genres
+    form_class = forms.GenresForm
+    template_name = 'app_reference_book/genres_update.html'
+    success_url = '/genres/'
+
+#DELETE
+class DeleteGenres(generic.DeleteView):
+    model = models.Genres
+    template_name = 'app_reference_book/genres_delete.html'
+    success_url = '/genres/'
+
+#LIST
+class ListGenres(generic.ListView):
+    model = models.Genres
+    template_name = 'app_reference_book/genres_list.html'
+
+###########################CRUDL-Series#################################
+#CREATE 
+class CreateSeries(generic.CreateView):
+    model = models.Series
+    form_class = forms.SeriesForm
+    template_name = 'app_reference_book/series_create.html'
+    success_url = '/series/'
+
+#READ
+class ReadSeries(generic.DetailView):
+    model = models.Series
+    template_name = 'app_reference_book/series_read.html'
+
+#UPDATE
+class UpdateSeries(generic.UpdateView):
+    model = models.Series
+    form_class = forms.SeriesForm
+    template_name = 'app_reference_book/series_update.html'
+    success_url = '/series/'
+
+#DELETE
+class DeleteSeries(generic.DeleteView):
+    model = models.Series
+    template_name = 'app_reference_book/series_delete.html'
+    success_url = '/series/'
+
+#LIST
+class ListSeries(generic.ListView):
+    model = models.Series
+    template_name = 'app_reference_book/series_list.html'
