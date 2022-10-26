417ce4bc43012bfcd415d73263556d0fcc6e55a1
IgorMakarevich <grigorichok@gmail.com>
2022-10-26 16:30:37 +0300

    add django teampleate base

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = ''
 
 
 # Default primary key field type
